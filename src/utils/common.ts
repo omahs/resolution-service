ed3e80fa1db0529c0cff2a7431d3575de70606b0
Kenneth Rahn Jr <kenneth@unstoppabledomains.com>
2023-02-15 17:52:02 -0500

    Whitespace change (#288)

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,4 +2,5 @@ export function convertToArray<T>(val: T | T[]): T[] {
   return Array.isArray(val) ? val : [val];
 }
 
+// adding a timeout to help mitigate against long-running external dependencies
 export const PROFILE_FETCH_TIMEOUT_MS = 200;
